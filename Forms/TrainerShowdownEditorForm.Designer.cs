75f45fc03ebbe6c1e7625af779d4e75bed250fd5
Nifyr <nintendofyr@gmail.com>
2022-06-04 03:05:02 +0200

    Made Showdown converter resizable

--- Forms/TrainerShowdownEditorForm.Designer.cs
+++ Forms/TrainerShowdownEditorForm.Designer.cs
@@ -35,29 +35,35 @@ namespace ImpostersOrdeal
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.Save = new System.Windows.Forms.Button();
             this.Preview = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(384, 461);
+            this.richTextBox1.Size = new System.Drawing.Size(402, 374);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "Paste Showdown Here";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(402, 12);
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(411, 3);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.Size = new System.Drawing.Size(384, 461);
+            this.richTextBox2.Size = new System.Drawing.Size(402, 374);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "Preview";
             // 
             // Save
             // 
-            this.Save.Location = new System.Drawing.Point(580, 479);
+            this.Save.Location = new System.Drawing.Point(3, 3);
             this.Save.Name = "Save";
             this.Save.Size = new System.Drawing.Size(100, 29);
             this.Save.TabIndex = 2;
@@ -67,7 +73,7 @@ namespace ImpostersOrdeal
             // 
             // Preview
             // 
-            this.Preview.Location = new System.Drawing.Point(686, 479);
+            this.Preview.Location = new System.Drawing.Point(109, 3);
             this.Preview.Name = "Preview";
             this.Preview.Size = new System.Drawing.Size(100, 29);
             this.Preview.TabIndex = 3;
@@ -75,23 +81,60 @@ namespace ImpostersOrdeal
             this.Preview.UseVisualStyleBackColor = true;
             this.Preview.Click += new System.EventHandler(this.Preview_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 9);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 467F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(816, 380);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.Save, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Preview, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(613, 389);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(212, 35);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
             // TrainerShowdownEditorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 520);
-            this.Controls.Add(this.Preview);
-            this.Controls.Add(this.Save);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.ClientSize = new System.Drawing.Size(834, 433);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "TrainerShowdownEditorForm";
             this.Text = "TrainerShowdownEditorForm";
             this.Load += new System.EventHandler(this.OnLoad);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,5 +144,7 @@ namespace ImpostersOrdeal
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button Save;
         private System.Windows.Forms.Button Preview;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
