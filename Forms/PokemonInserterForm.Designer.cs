835dce226a510f610de3dd4c7bd8d401cd40f127
Nifyr <nintendofyr@gmail.com>
2022-10-07 18:26:13 +0200

    Redesigned pokémon inserter tool

--- Forms/PokemonInserterForm.Designer.cs
+++ Forms/PokemonInserterForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PokemonInserterForm));
-            this.dexIDComboBox = new System.Windows.Forms.ComboBox();
+            this.srcDexIDComboBox = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.formIDComboBox = new System.Windows.Forms.ComboBox();
@@ -39,59 +39,70 @@
             this.newSpeciesRadioButton = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.speciesNameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.genderConfigTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dstDexIDComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dexIDComboBox
+            // srcDexIDComboBox
             // 
-            this.dexIDComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
-            this.dexIDComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.dexIDComboBox.FormattingEnabled = true;
-            this.dexIDComboBox.Location = new System.Drawing.Point(12, 12);
-            this.dexIDComboBox.Name = "dexIDComboBox";
-            this.dexIDComboBox.Size = new System.Drawing.Size(189, 28);
-            this.dexIDComboBox.TabIndex = 0;
+            this.srcDexIDComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.srcDexIDComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.srcDexIDComboBox.FormattingEnabled = true;
+            this.srcDexIDComboBox.Location = new System.Drawing.Point(83, 26);
+            this.srcDexIDComboBox.Name = "srcDexIDComboBox";
+            this.srcDexIDComboBox.Size = new System.Drawing.Size(100, 28);
+            this.srcDexIDComboBox.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 246);
+            this.button1.Location = new System.Drawing.Point(12, 429);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(189, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Clone and Insert Form";
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clone and Insert";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.AddFormClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Location = new System.Drawing.Point(69, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.Size = new System.Drawing.Size(58, 20);
             this.label1.TabIndex = 3;
-            this.label1.Text = "FormID to Clone";
+            this.label1.Text = "FormID";
             // 
             // formIDComboBox
             // 
             this.formIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.formIDComboBox.FormattingEnabled = true;
-            this.formIDComboBox.Location = new System.Drawing.Point(151, 46);
+            this.formIDComboBox.Location = new System.Drawing.Point(133, 60);
             this.formIDComboBox.Name = "formIDComboBox";
             this.formIDComboBox.Size = new System.Drawing.Size(50, 28);
             this.formIDComboBox.TabIndex = 4;
             // 
             // formNameTextBox
             // 
-            this.formNameTextBox.Location = new System.Drawing.Point(12, 213);
+            this.formNameTextBox.Location = new System.Drawing.Point(6, 133);
             this.formNameTextBox.Name = "formNameTextBox";
-            this.formNameTextBox.Size = new System.Drawing.Size(189, 27);
-            this.formNameTextBox.TabIndex = 7;
+            this.formNameTextBox.Size = new System.Drawing.Size(177, 27);
+            this.formNameTextBox.TabIndex = 16;
             this.formNameTextBox.Text = "New Form";
             this.formNameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Location = new System.Drawing.Point(6, 110);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(118, 20);
             this.label3.TabIndex = 8;
@@ -100,7 +111,7 @@
             // newFormRadioButton
             // 
             this.newFormRadioButton.AutoSize = true;
-            this.newFormRadioButton.Location = new System.Drawing.Point(103, 80);
+            this.newFormRadioButton.Location = new System.Drawing.Point(85, 26);
             this.newFormRadioButton.Name = "newFormRadioButton";
             this.newFormRadioButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.newFormRadioButton.Size = new System.Drawing.Size(98, 24);
@@ -111,7 +122,7 @@
             // newSpeciesRadioButton
             // 
             this.newSpeciesRadioButton.AutoSize = true;
-            this.newSpeciesRadioButton.Location = new System.Drawing.Point(87, 110);
+            this.newSpeciesRadioButton.Location = new System.Drawing.Point(69, 56);
             this.newSpeciesRadioButton.Name = "newSpeciesRadioButton";
             this.newSpeciesRadioButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.newSpeciesRadioButton.Size = new System.Drawing.Size(114, 24);
@@ -122,7 +133,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Location = new System.Drawing.Point(6, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(148, 20);
             this.label2.TabIndex = 12;
@@ -130,41 +141,128 @@
             // 
             // speciesNameTextBox
             // 
-            this.speciesNameTextBox.Location = new System.Drawing.Point(12, 160);
+            this.speciesNameTextBox.Location = new System.Drawing.Point(6, 80);
             this.speciesNameTextBox.Name = "speciesNameTextBox";
-            this.speciesNameTextBox.Size = new System.Drawing.Size(189, 27);
-            this.speciesNameTextBox.TabIndex = 11;
+            this.speciesNameTextBox.Size = new System.Drawing.Size(177, 27);
+            this.speciesNameTextBox.TabIndex = 15;
             this.speciesNameTextBox.Text = "New Species";
             this.speciesNameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Species";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.genderConfigTextBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.srcDexIDComboBox);
+            this.groupBox1.Controls.Add(this.formIDComboBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 147);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source Pokémon";
+            // 
+            // genderConfigTextBox
+            // 
+            this.genderConfigTextBox.Location = new System.Drawing.Point(6, 114);
+            this.genderConfigTextBox.Name = "genderConfigTextBox";
+            this.genderConfigTextBox.ReadOnly = true;
+            this.genderConfigTextBox.Size = new System.Drawing.Size(177, 27);
+            this.genderConfigTextBox.TabIndex = 15;
+            this.genderConfigTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Gender Config";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.newFormRadioButton);
+            this.groupBox2.Controls.Add(this.newSpeciesRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(189, 86);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mode";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.dstDexIDComboBox);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.formNameTextBox);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.speciesNameTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 257);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(189, 166);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Target";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Species";
+            // 
+            // dstDexIDComboBox
+            // 
+            this.dstDexIDComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.dstDexIDComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.dstDexIDComboBox.FormattingEnabled = true;
+            this.dstDexIDComboBox.Location = new System.Drawing.Point(83, 26);
+            this.dstDexIDComboBox.Name = "dstDexIDComboBox";
+            this.dstDexIDComboBox.Size = new System.Drawing.Size(100, 28);
+            this.dstDexIDComboBox.TabIndex = 14;
             // 
             // PokemonInserterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(213, 287);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.speciesNameTextBox);
-            this.Controls.Add(this.newSpeciesRadioButton);
-            this.Controls.Add(this.newFormRadioButton);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.formNameTextBox);
-            this.Controls.Add(this.formIDComboBox);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(213, 470);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dexIDComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PokemonInserterForm";
             this.Text = "Pokémon Inserter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox dexIDComboBox;
+        private System.Windows.Forms.ComboBox srcDexIDComboBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox formIDComboBox;
@@ -174,5 +272,13 @@
         private System.Windows.Forms.RadioButton newSpeciesRadioButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox speciesNameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox dstDexIDComboBox;
+        private System.Windows.Forms.TextBox genderConfigTextBox;
+        private System.Windows.Forms.Label label6;
     }
 }
--- Forms/PokemonInserterForm.cs
+++ Forms/PokemonInserterForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static ImpostersOrdeal.GameDataTypes;
+using static ImpostersOrdeal.Masterdatas;
 using static ImpostersOrdeal.GlobalData;
 
 namespace ImpostersOrdeal
@@ -15,12 +16,18 @@ namespace ImpostersOrdeal
     public partial class PokemonInserterForm : Form
     {
         private List<DexEntry> dexEntries;
-        private DexEntry d;
+        private DexEntry srcDE;
+        private DexEntry dstDE;
         private InserterMode inserterMode;
 
         private enum InserterMode
         {
             Form, Species
+        }
+
+        private enum GenderConfig
+        {
+            None, MaleOnly, FemaleOnly, Normal, Variations, Genderless
         }
 
         public PokemonInserterForm()
@@ -30,24 +37,50 @@ namespace ImpostersOrdeal
 
             InitializeComponent();
 
-            d = dexEntries[0];
-            dexIDComboBox.DataSource = dexEntries.Select(o => o.GetName()).ToArray();
-            dexIDComboBox.SelectedIndex = 0;
+            srcDE = dexEntries[0];
+            srcDexIDComboBox.DataSource = dexEntries.Select(o => o.GetName()).ToArray();
+            srcDexIDComboBox.SelectedIndex = 0;
+
+            dstDE = dexEntries[0];
+            dstDexIDComboBox.DataSource = dexEntries.Select(o => o.GetName()).ToArray();
+            dstDexIDComboBox.SelectedIndex = 0;
 
             newFormRadioButton.Checked = true;
             label2.Text = "Pokémon " + dexEntries.Count + " Name:";
 
-            RefreshDexEntryDisplay();
+            RefreshSrcDexEntryDisplay();
+            RefreshGenderInfoDisplay();
+            RefreshDstDexEntryDisplay();
             RefreshModeDisplay();
             ActivateControls();
         }
 
-        private void DexEntryChanged(object sender, EventArgs e)
+        private void SrcDexEntryChanged(object sender, EventArgs e)
         {
             DeactivateControls();
 
-            d = dexEntries[dexIDComboBox.SelectedIndex];
-            RefreshDexEntryDisplay();
+            srcDE = dexEntries[srcDexIDComboBox.SelectedIndex];
+            RefreshSrcDexEntryDisplay();
+            RefreshGenderInfoDisplay();
+
+            ActivateControls();
+        }
+
+        private void FormIDChanged(object sender, EventArgs e)
+        {
+            DeactivateControls();
+
+            RefreshGenderInfoDisplay();
+
+            ActivateControls();
+        }
+
+        private void DstDexEntryChanged(object sender, EventArgs e)
+        {
+            DeactivateControls();
+
+            dstDE = dexEntries[dstDexIDComboBox.SelectedIndex];
+            RefreshDstDexEntryDisplay();
 
             ActivateControls();
         }
@@ -62,18 +95,37 @@ namespace ImpostersOrdeal
             ActivateControls();
         }
 
-        private void RefreshDexEntryDisplay()
+        private void RefreshSrcDexEntryDisplay()
         {
-            formIDComboBox.DataSource = d.forms.Select((o, i) => i).ToArray();
+            formIDComboBox.DataSource = srcDE.forms.Select((o, i) => i).ToArray();
             formIDComboBox.SelectedIndex = 0;
-            label3.Text = inserterMode == InserterMode.Form ? "Form " + d.forms.Count + " Name:" : "Form 0 Name:";
+        }
+
+        private void RefreshGenderInfoDisplay()
+        {
+            switch (GetGenderConfig(srcDE.dexID, formIDComboBox.SelectedIndex))
+            {
+                case GenderConfig.None: genderConfigTextBox.Text = "None"; break;
+                case GenderConfig.MaleOnly: genderConfigTextBox.Text = "Male only"; break;
+                case GenderConfig.FemaleOnly: genderConfigTextBox.Text = "Female only"; break;
+                case GenderConfig.Normal: genderConfigTextBox.Text = "Male/Female"; break;
+                case GenderConfig.Variations: genderConfigTextBox.Text = "Male/Female variants"; break;
+                case GenderConfig.Genderless: genderConfigTextBox.Text = "Genderless"; break;
+            }
+        }
+
+        private void RefreshDstDexEntryDisplay()
+        {
+            label3.Text = inserterMode == InserterMode.Form ? "Form " + dstDE.forms.Count + " Name:" : "Form 0 Name:";
         }
 
         private void RefreshModeDisplay()
         {
+            dstDexIDComboBox.Enabled = inserterMode == InserterMode.Form;
+            label5.Enabled = inserterMode == InserterMode.Form;
             speciesNameTextBox.Enabled = inserterMode == InserterMode.Species;
             label2.Enabled = inserterMode == InserterMode.Species;
-            label3.Text = inserterMode == InserterMode.Form ? "Form " + d.forms.Count + " Name:" : "Form 0 Name:";
+            label3.Text = inserterMode == InserterMode.Form ? "Form " + dstDE.forms.Count + " Name:" : "Form 0 Name:";
             if (inserterMode == InserterMode.Species)
                 formNameTextBox.Text = "";
             else
@@ -82,21 +134,39 @@ namespace ImpostersOrdeal
 
         private void ActivateControls()
         {
-            dexIDComboBox.SelectedIndexChanged += DexEntryChanged;
+            srcDexIDComboBox.SelectedIndexChanged += SrcDexEntryChanged;
+            formIDComboBox.SelectedIndexChanged += FormIDChanged;
+            dstDexIDComboBox.SelectedIndexChanged += DstDexEntryChanged;
             newFormRadioButton.CheckedChanged += ModeChanged;
             newSpeciesRadioButton.CheckedChanged += ModeChanged;
         }
 
         private void DeactivateControls()
         {
-            dexIDComboBox.SelectedIndexChanged -= DexEntryChanged;
+            srcDexIDComboBox.SelectedIndexChanged -= SrcDexEntryChanged;
+            formIDComboBox.SelectedIndexChanged -= FormIDChanged;
+            dstDexIDComboBox.SelectedIndexChanged -= DstDexEntryChanged;
             newFormRadioButton.CheckedChanged -= ModeChanged;
             newSpeciesRadioButton.CheckedChanged -= ModeChanged;
         }
 
         private void AddFormClick(object sender, EventArgs e)
         {
-            if (d.dexID == 0)
+            if (srcDexIDComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("I have never heard of this species...\n\"" + srcDexIDComboBox.Text + "\"",
+                    "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (inserterMode == InserterMode.Form && dstDexIDComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("I have never heard of this species...\n\"" + dstDexIDComboBox.Text + "\"",
+                    "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (srcDE.dexID == 0 || dstDE.dexID == 0)
             {
                 MessageBox.Show("I'm not gonna insert a damn egg.\nAnd I couldn't even if I wanted to.",
                     "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -110,19 +180,43 @@ namespace ImpostersOrdeal
 
             if (inserterMode == InserterMode.Form)
             {
-                PokemonInserter.GetInstance().InsertPokemon(d.dexID, d.dexID, (int)formIDComboBox.SelectedItem, d.forms.Count, speciesNameTextBox.Text, formNameTextBox.Text);
-                MessageBox.Show("Data for " + d.GetName() + " " + formNameTextBox.Text + " has been inserted!",
+                PokemonInserter.GetInstance().InsertPokemon(srcDE.dexID, dstDE.dexID, (int)formIDComboBox.SelectedItem, dstDE.forms.Count, speciesNameTextBox.Text, formNameTextBox.Text);
+                MessageBox.Show("Data for " + dstDE.GetName() + " " + formNameTextBox.Text + " has been inserted!",
                     "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                PokemonInserter.GetInstance().InsertPokemon(d.dexID, dexEntries.Count, (int)formIDComboBox.SelectedItem, 0, speciesNameTextBox.Text, formNameTextBox.Text);
+                PokemonInserter.GetInstance().InsertPokemon(srcDE.dexID, dexEntries.Count, (int)formIDComboBox.SelectedItem, 0, speciesNameTextBox.Text, formNameTextBox.Text);
                 MessageBox.Show("Data for " + speciesNameTextBox.Text + " has been inserted!",
                     "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             label2.Text = "Pokémon " + dexEntries.Count + " Name:";
             speciesNameTextBox.Text = "New Species";
-            dexIDComboBox.DataSource = dexEntries.Select(o => o.GetName()).ToArray();
+            int srcIdx = srcDexIDComboBox.SelectedIndex;
+            int dstIdx = dstDexIDComboBox.SelectedIndex;
+            srcDexIDComboBox.DataSource = dexEntries.Select(o => o.GetName()).ToArray();
+            dstDexIDComboBox.DataSource = dexEntries.Select(o => o.GetName()).ToArray();
+            srcDexIDComboBox.SelectedIndex = srcIdx;
+            dstDexIDComboBox.SelectedIndex = dstIdx;
+        }
+
+        private GenderConfig GetGenderConfig(int dexID, int formID)
+        {
+            List<PokemonInfoCatalog> pics = gameData.pokemonInfos.Where(pic => pic.MonsNo == dexID && pic.FormNo == formID && !pic.Rare).ToList();
+            if (pics.Count == 0)
+                return GenderConfig.None;
+            if (pics.Count > 2)
+                throw new ArgumentException("Too many PokemonInfo entries found for dexID " + dexID + " and formID " + formID);
+            if (pics.Count == 1)
+                switch (pics.First().Sex)
+                {
+                    case 0: return GenderConfig.MaleOnly;
+                    case 1: return GenderConfig.FemaleOnly;
+                    case 2: return GenderConfig.Genderless;
+                }
+            if (pics[0].AssetBundleName == pics[1].AssetBundleName)
+                return GenderConfig.Normal;
+            return GenderConfig.Variations;
         }
     }
 }
