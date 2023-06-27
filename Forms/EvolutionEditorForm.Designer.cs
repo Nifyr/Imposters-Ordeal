369f01b01a9290f2d59c501c33cbc9ae4a30019d
Nifyr <nintendofyr@gmail.com>
2023-06-27 22:11:37 +0200

    Made evolution editor window resizeable

--- Forms/EvolutionEditorForm.Designer.cs
+++ Forms/EvolutionEditorForm.Designer.cs
@@ -30,127 +30,149 @@ namespace ImpostersOrdeal
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EvolutionEditorForm));
-            this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.destinationDexIDColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.methodColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
-            this.formIDComboBox = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lvReqNumericUpDown = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.evoParamComboBox = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lvReqNumericUpDown)).BeginInit();
-            this.SuspendLayout();
+            dataGridView = new System.Windows.Forms.DataGridView();
+            destinationDexIDColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            methodColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            label1 = new System.Windows.Forms.Label();
+            formIDComboBox = new System.Windows.Forms.ComboBox();
+            label2 = new System.Windows.Forms.Label();
+            lvReqNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            label3 = new System.Windows.Forms.Label();
+            evoParamComboBox = new System.Windows.Forms.ComboBox();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)lvReqNumericUpDown).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // dataGridView
             // 
-            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.destinationDexIDColumn,
-            this.methodColumn});
-            this.dataGridView.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.RowHeadersWidth = 20;
-            this.dataGridView.RowTemplate.Height = 29;
-            this.dataGridView.Size = new System.Drawing.Size(403, 289);
-            this.dataGridView.TabIndex = 0;
+            dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { destinationDexIDColumn, methodColumn });
+            dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            dataGridView.Location = new System.Drawing.Point(3, 3);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 20;
+            dataGridView.RowTemplate.Height = 29;
+            dataGridView.Size = new System.Drawing.Size(416, 307);
+            dataGridView.TabIndex = 0;
             // 
             // destinationDexIDColumn
             // 
-            this.destinationDexIDColumn.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.Nothing;
-            this.destinationDexIDColumn.HeaderText = "Destination";
-            this.destinationDexIDColumn.MinimumWidth = 6;
-            this.destinationDexIDColumn.Name = "destinationDexIDColumn";
+            destinationDexIDColumn.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.Nothing;
+            destinationDexIDColumn.HeaderText = "Destination";
+            destinationDexIDColumn.MinimumWidth = 6;
+            destinationDexIDColumn.Name = "destinationDexIDColumn";
             // 
             // methodColumn
             // 
-            this.methodColumn.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
-            this.methodColumn.FillWeight = 300F;
-            this.methodColumn.HeaderText = "Method";
-            this.methodColumn.MinimumWidth = 6;
-            this.methodColumn.Name = "methodColumn";
+            methodColumn.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
+            methodColumn.FillWeight = 300F;
+            methodColumn.HeaderText = "Method";
+            methodColumn.MinimumWidth = 6;
+            methodColumn.Name = "methodColumn";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(421, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Destination Form";
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(123, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Destination Form";
             // 
             // formIDComboBox
             // 
-            this.formIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.formIDComboBox.FormattingEnabled = true;
-            this.formIDComboBox.Location = new System.Drawing.Point(421, 32);
-            this.formIDComboBox.Name = "formIDComboBox";
-            this.formIDComboBox.Size = new System.Drawing.Size(189, 28);
-            this.formIDComboBox.TabIndex = 2;
+            formIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            formIDComboBox.FormattingEnabled = true;
+            formIDComboBox.Location = new System.Drawing.Point(3, 23);
+            formIDComboBox.Name = "formIDComboBox";
+            formIDComboBox.Size = new System.Drawing.Size(189, 28);
+            formIDComboBox.TabIndex = 2;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(421, 63);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Level Requirement";
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(3, 54);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(132, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Level Requirement";
             // 
             // lvReqNumericUpDown
             // 
-            this.lvReqNumericUpDown.Location = new System.Drawing.Point(421, 86);
-            this.lvReqNumericUpDown.Maximum = new decimal(new int[] {
-            65535,
-            0,
-            0,
-            0});
-            this.lvReqNumericUpDown.Name = "lvReqNumericUpDown";
-            this.lvReqNumericUpDown.Size = new System.Drawing.Size(189, 27);
-            this.lvReqNumericUpDown.TabIndex = 4;
+            lvReqNumericUpDown.Location = new System.Drawing.Point(3, 77);
+            lvReqNumericUpDown.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            lvReqNumericUpDown.Name = "lvReqNumericUpDown";
+            lvReqNumericUpDown.Size = new System.Drawing.Size(189, 27);
+            lvReqNumericUpDown.TabIndex = 4;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(421, 116);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Param";
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(3, 107);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(50, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Param";
             // 
             // evoParamComboBox
             // 
-            this.evoParamComboBox.FormattingEnabled = true;
-            this.evoParamComboBox.Location = new System.Drawing.Point(421, 139);
-            this.evoParamComboBox.Name = "evoParamComboBox";
-            this.evoParamComboBox.Size = new System.Drawing.Size(189, 28);
-            this.evoParamComboBox.TabIndex = 6;
+            evoParamComboBox.FormattingEnabled = true;
+            evoParamComboBox.Location = new System.Drawing.Point(3, 130);
+            evoParamComboBox.Name = "evoParamComboBox";
+            evoParamComboBox.Size = new System.Drawing.Size(189, 28);
+            evoParamComboBox.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            tableLayoutPanel1.Controls.Add(dataGridView, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 1, 0);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(622, 313);
+            tableLayoutPanel1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(evoParamComboBox);
+            panel1.Controls.Add(formIDComboBox);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(lvReqNumericUpDown);
+            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Location = new System.Drawing.Point(425, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(194, 307);
+            panel1.TabIndex = 1;
             // 
             // EvolutionEditorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 313);
-            this.Controls.Add(this.evoParamComboBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.lvReqNumericUpDown);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.formIDComboBox);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "EvolutionEditorForm";
-            this.Text = "EvolutionEditorForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lvReqNumericUpDown)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(622, 313);
+            Controls.Add(tableLayoutPanel1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "EvolutionEditorForm";
+            Text = "EvolutionEditorForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)lvReqNumericUpDown).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
@@ -164,5 +186,7 @@ namespace ImpostersOrdeal
         private System.Windows.Forms.ComboBox evoParamComboBox;
         private System.Windows.Forms.DataGridViewComboBoxColumn destinationDexIDColumn;
         private System.Windows.Forms.DataGridViewComboBoxColumn methodColumn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
